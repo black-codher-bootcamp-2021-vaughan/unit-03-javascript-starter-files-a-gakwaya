f4dd766ad3c4c0b69c864529e08967ffb1442c65
a-gakwaya <agakwaya@gmail.com>
2021-11-29 19:23:19 +0000

    Update index.js

--- session-3/index.js
+++ session-3/index.js
@@ -1,2 +1,7 @@
 // This is a comment in JavaScript
 // You can see where this file is connected to your HTML in the index.html on line 14
+
+let a = 5; 
+let b = 10; 
+const c = a + b; 
+console.log('Original ouput: ' + c)
